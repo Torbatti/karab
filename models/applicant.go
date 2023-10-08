ee135de8e03d969631fb6a3c8a8b41bc51e5c178
torbatti <torbattii@gmail.com>
2023-10-08 08:07:39 +0330

    simplified applicant fields

--- models/applicant.go
+++ models/applicant.go
@@ -3,9 +3,7 @@ package models
 import "time"
 
 type Applicant struct {
-	ID              uint `json:"id" gorm:"primaryKey"`
-	CreatedAt       time.Time
-	ApplicantName   string `json:"applicant_name"`
-	ApplicantAge    uint   `json:"applicant_age"`
-	ApplicantResume string `json:"applicant_resume"`
+	ID            uint `json:"id" gorm:"primaryKey"`
+	CreatedAt     time.Time
+	ApplicantName string `json:"applicant_name"`
 }
