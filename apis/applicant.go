b954820f26d5df34ab70d158658445e9ef41ecf4
torbatti <torbattii@gmail.com>
2023-10-08 09:22:16 +0330

    get applicants

--- apis/applicant.go
+++ apis/applicant.go
@@ -1,6 +1,8 @@
 package apis
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/torbatti/karab/core"
 	"github.com/torbatti/karab/models"
@@ -25,6 +27,14 @@ func CreateResponseApplicant(apl models.Applicant) Applicant {
 	return Applicant{apl.ID, apl.ApplicantName}
 }
 
+func findApplicant(id int, apl *models.Applicant) error {
+	core.DataBase.Db.Find(&apl, "id = ?", id)
+	if apl.ID == 0 {
+		return errors.New("Applicant Doesn't Exists")
+	}
+	return nil
+}
+
 func CreateApplicant(c *fiber.Ctx) error {
 	var apl models.Applicant
 
@@ -34,6 +44,36 @@ func CreateApplicant(c *fiber.Ctx) error {
 	}
 	core.DataBase.Db.Create(&apl)
 
+	respApl := CreateResponseApplicant(apl)
+
+	return c.Status(200).JSON(respApl)
+}
+
+func GetApplicants(c *fiber.Ctx) error {
+	apls := []models.Applicant{}
+
+	core.DataBase.Db.Find(&apls)
+	respApls := []Applicant{}
+
+	for _, apl := range apls {
+		respApl := CreateResponseApplicant(apl)
+		respApls = append(respApls, respApl)
+	}
+
+	return c.Status(200).JSON(respApls)
+}
+
+func GetApplicant(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		// TODO : BETTER ERROR HANDLING AND VALIDATION
+		return c.Status(400).JSON("ID is not provided correctly")
+	}
+
+	var apl models.Applicant
+	if err := findApplicant(id, &apl); err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
 	respApl := CreateResponseApplicant(apl)
 
 	return c.Status(200).JSON(respApl)
