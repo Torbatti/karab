fb608367a9594202bdef3360097f27b72079a59f
torbatti <torbattii@gmail.com>
2023-10-18 19:22:34 +0330

    update:added html parser for views

--- karab.go
+++ karab.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"html/template"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
 	"github.com/torbatti/karab/apis"
@@ -10,8 +12,14 @@ import (
 const Version = "00.00.01"
 
 func main() {
-	engine := html.New("./views", ".html")
 	core.ConnectDb()
+	engine := html.New("./views", ".html")
+	engine.AddFunc(
+		// add unescape function
+		"unescape", func(s string) template.HTML {
+			return template.HTML(s)
+		},
+	)
 
 	app := fiber.New(fiber.Config{
 		Views: engine,
