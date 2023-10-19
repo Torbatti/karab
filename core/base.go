68869c726d1846349acd898f9d5af249e8dc0475
torbatti <torbattii@gmail.com>
2023-10-19 20:56:02 +0330

    update:replaced headTitle

--- core/base.go
+++ core/base.go
@@ -7,7 +7,7 @@ import (
 func Base(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("pages/index", fiber.Map{
-			"HeadTitle":   "Karab",
+			"HeadTitle":   "کاراب",
 			"KeyWords":    "KeyWords",
 			"Description": "Description",
 		}, "layouts/main")
