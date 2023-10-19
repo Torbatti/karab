877cdb3476736dcb0ab8fa93a736ac9c044d631b
torbatti <torbattii@gmail.com>
2023-10-19 20:56:27 +0330

    update: added /jobs route

--- core/job.go
+++ core/job.go
@@ -3,4 +3,11 @@ package core
 import "github.com/gofiber/fiber/v2"
 
 func Job(app *fiber.App) {
+	app.Get("/jobs", func(c *fiber.Ctx) error {
+		return c.Render("pages/jobs", fiber.Map{
+			"HeadTitle":   "فرصت های شغلی",
+			"KeyWords":    "KeyWords",
+			"Description": "Description",
+		}, "layouts/main")
+	})
 }
