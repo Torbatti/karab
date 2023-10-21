8900a2a5cf835ccde3533541001d715c8398173f
torbatti <torbattii@gmail.com>
2023-10-21 23:11:46 +0330

    removed wrongly placed route

--- routes/job.go
+++ routes/job.go
@@ -3,11 +3,5 @@ package routes
 import "github.com/gofiber/fiber/v2"
 
 func Job(app *fiber.App) {
-	app.Get("/jobs", func(c *fiber.Ctx) error {
-		return c.Render("pages/jobs", fiber.Map{
-			"HeadTitle":   "فرصت های شغلی",
-			"KeyWords":    "KeyWords",
-			"Description": "Description",
-		}, "layouts/main")
-	})
+
 }
