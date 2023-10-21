8f29fc298c478b1a5d7bd914f3a225df65ef41ce
torbatti <torbattii@gmail.com>
2023-10-21 22:26:29 +0330

    splited auth function into login and signup

--- hx/auth.go
+++ hx/auth.go
@@ -14,30 +14,10 @@ type AuthForm struct {
 }
 
 func Auth(app *fiber.App) {
-	app.Post("/hx/login-btn", func(c *fiber.Ctx) error {
-		l := new(AuthForm)
-		if err := c.BodyParser(l); err != nil {
-			return err
-		}
-		log.Println(l.UserName)
-		log.Println(l.Password)
 
-		// Find User
-		var apl models.Applicant
-		if err := core.DataBase.Db.Find(&apl, "applicant_name = ?", l.UserName).Error; err != nil {
-			log.Println(err)
-		}
-		log.Println(apl.CreatedAt)
+}
 
-		// IMPLEMENT BASIC AUTH MIDDLEWARE
-		// c.Set("HX-Push-Url", "/")
-		c.Set("HX-Redirect", "/")
-
-		log.Println(c.GetRespHeaders())
-
-		// ONLY IF LOGGED IN RETURN 200
-		return c.SendStatus(200)
-	})
+func AuthSignup(app *fiber.App) {
 
 	app.Post("/hx/signup-btn", func(c *fiber.Ctx) error {
 		l := new(AuthForm)
@@ -64,4 +44,31 @@ func Auth(app *fiber.App) {
 		return c.SendStatus(200)
 	})
 
+}
+
+func AuthLogin(app *fiber.App) {
+	app.Post("/hx/login-btn", func(c *fiber.Ctx) error {
+		l := new(AuthForm)
+		if err := c.BodyParser(l); err != nil {
+			return err
+		}
+		log.Println(l.UserName)
+		log.Println(l.Password)
+
+		// Find User
+		var apl models.Applicant
+		if err := core.DataBase.Db.Find(&apl, "applicant_name = ?", l.UserName).Error; err != nil {
+			log.Println(err)
+		}
+		log.Println(apl.CreatedAt)
+
+		// IMPLEMENT BASIC AUTH MIDDLEWARE
+		// c.Set("HX-Push-Url", "/")
+		c.Set("HX-Redirect", "/")
+
+		log.Println(c.GetRespHeaders())
+
+		// ONLY IF LOGGED IN RETURN 200
+		return c.SendStatus(200)
+	})
 }
